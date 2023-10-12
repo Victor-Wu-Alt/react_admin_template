ca292ede89ff5197b33acd0bb0ac4d0c1824eb1a
victor <wuguangwei@tsingjet.com>
2023-10-12 10:52:48 +0800

    Updates

--- assets/index-e7f7570c.js
+++
@@ -1,1 +0,0 @@
-import{aK as s,ak as e}from"./index-155912ce.js";const a=()=>s.jsxs(s.Fragment,{children:[s.jsx(e,{children:"User"}),s.jsx("p",{className:"red",children:"测试scss"})]});export{a as default};
